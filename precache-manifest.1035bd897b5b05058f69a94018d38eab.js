d43c19e2dbe69e00895ba3879baf7f5271e82ce0
uahmed23 <umer@uoguelph.ca>
2020-09-05 22:37:41 -0400

    Updates

---
+++ precache-manifest.1035bd897b5b05058f69a94018d38eab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e5dc2d8018aced4f45659269418fe5",
+    "url": "/pan-pals/index.html"
+  },
+  {
+    "revision": "3c8609e312939e923ab2",
+    "url": "/pan-pals/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "36089f2f33b7f3b299be",
+    "url": "/pan-pals/static/css/main.8e5657f1.chunk.css"
+  },
+  {
+    "revision": "3c8609e312939e923ab2",
+    "url": "/pan-pals/static/js/2.e305715d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pan-pals/static/js/2.e305715d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36089f2f33b7f3b299be",
+    "url": "/pan-pals/static/js/main.328a9555.chunk.js"
+  },
+  {
+    "revision": "51e1c89df2e06231aea8",
+    "url": "/pan-pals/static/js/runtime-main.0ec9009d.js"
+  }
+]);
